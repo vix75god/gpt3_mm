f024971cf92d4a6fc7773344fc543757903d1810
vix75god <musarurwamunya@gmail.com>
2023-06-26 12:56:11 -0700

    added some styles and updated the jsx files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__nabar-links">
+        <div className="gpt3__navbar-links_logo"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
