32cfa45324d67454e6d0b3fc303e5847c6e05279
vix75god <musarurwamunya@gmail.com>
2023-06-26 14:35:32 -0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,6 +10,15 @@ const Navbar = () => {
         <div className="gpt3__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+            <a href="#wgpt3">What is GPT3?</a>
+            <a href="#possibility">Open AI</a>
+            <a href="#features">Case Studies</a>
+            <a href="#blog">Library</a>
+          </p>
+        </div>
       </div>
     </div>
   );
