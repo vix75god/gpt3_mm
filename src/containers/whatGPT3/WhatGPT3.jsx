2d61bb90aebca069147c3dc1c0cd5b22a100f2df
vix75god <musarurwamunya@gmail.com>
2023-07-06 16:56:52 +1200

    added the whatgpt3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,38 @@
 import React from "react";
 import "./whatGPT3.css";
+import { Feature } from "../../components";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__padding" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="We are such good friends, and receiving your message brings me delight. I heard the entire story, as it unfolded in front of me. Oh, I should have known better. Neither his defective residence nor his own conviction convinced me. The connection has made it impossible to have my own boisterous apartment. At the jointure, the lady insisted on showing her humanity. As a friendly bachelor, I entered."
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+        <div className="gpt3__whatgpt3-container">
+          <Feature
+            title="Chatbots"
+            text="We share the opinion as friends, and receiving your message brings me delight. I heard the whole story in front of the plate, and oh, I ought to have known."
+          />
+          <Feature
+            title="Knowledgebase"
+            text="At the jointure, the ladyship insisted on showing her humanity, so he did. A friendly bachelor entered the scene. As for the impossible task of owning apartments."
+          />
+          <Feature
+            title="Education"
+            text="At the jointure, the ladyship insisted on showing her humanity, so he did. A friendly bachelor entered the scene. As for the impossible task of owning apartments."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
